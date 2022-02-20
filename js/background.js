82df76d36a0549e8c09e0634d33a9f18032a61a0
Redchrysanthemum <98815579+Redchrysanthemum@users.noreply.github.com>
2022-02-21 05:53:57 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -10,6 +10,6 @@ const chosenImage = images[Math.floor(Math.random() * images.length )];
 const bgimage = document.createElement("img");
 
 
-bgimage.src = `./img/${chosenImage}`;
+bgimage.src = `img/${chosenImage}`;
 
 document.body.appendChild(bgimage);
