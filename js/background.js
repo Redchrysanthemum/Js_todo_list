eb2db24ce514879964947caa59f8a921bf5d2119
Redchrysanthemum <98815579+Redchrysanthemum@users.noreply.github.com>
2022-02-21 06:00:38 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,15 @@
+const images = [
+    "bg1.JPG",
+    "bg2.JPG",
+    "bg3.JPG",
+    "bg4.JPG"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length )];
+
+const bgimage = document.createElement("img");
+
+
+bgimage.src = `./img/${chosenImage}`;
+
+document.body.appendChild(bgimage);
